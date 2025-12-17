07bb642d17d809bbc9dcc055ced51ac675d74030
Navdeep Saini <navdeep@mailbox.org>
2025-12-17 18:57:32 +0700

    backend: move to esm and tsx

--- apps/backend/src/trpc/router.ts
+++ apps/backend/src/trpc/router.ts
@@ -1,12 +1,10 @@
 // src/trpc/router.ts
 import { initTRPC } from "@trpc/server";
-import { plannerRouter } from "./routers/planner";
+// import { plannerRouter } from "./routers/planner";
 import type { Context } from "./context";
 
 const t = initTRPC.context<Context>().create();
 
-export const appRouter = t.router({
-  planner: plannerRouter,
-});
+export const appRouter = t.router({});
 
 export type AppRouter = typeof appRouter;
